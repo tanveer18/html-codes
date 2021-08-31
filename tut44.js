0c8803d8309e43128c4d236cfff67122de9d85a6
tanveer khan <tk5484030gmail.com>
2021-08-31 13:35:23 -0700

    add web development

---
+++ tut44.js
@@ -0,0 +1,34 @@
+console.log("This Is Tutorial 44");
+
+
+function greet(name, greetText) {
+    console.log(name + " Is a Good Boy ");
+    console.log(greetText + " " + name);
+}
+function sum(a, b, c) {
+    let d = a + b + c;
+    return d;
+}
+
+// let name1 = "tanveer"
+// let name2 = "razique"
+// let name3 = "wasif"
+// let name4 = "aabid"
+// let greetText = "Good Morning"
+
+// greet(name1);
+// greet(name2);
+// greet(name3);
+// greet(name4);
+// greet(name1, greetText);
+// greet(name2, greetText);
+// greet(name3, greetText);
+// greet(name4, greetText);
+
+
+let returnval = sum(1, 2, 3);
+console.log(returnval);
+// console.log(name + " Is a Good Boy")
+// console.log(name2 + " Is a Good Boy")
+// console.log(name3 + " Is a Good Boy")
+// console.log(name4 + " Is a Good Boy")
